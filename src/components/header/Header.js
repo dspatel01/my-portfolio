b95dac85f53a13e91df7ce2544bc888c6294ae9a
dileep <pateldileep290492@gmail.com>
2023-07-17 00:07:30 +0530

    hedder file

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,27 +1,12 @@
 import React, { useEffect } from 'react';
 import './header.scss';
-// import pic from '../../images/2-removebg-preview.png'
 import headerPic from '../../images/8401.jpg'
-// import resume from '../../extraFile/dileepspatel-070723.pdf'
-// import resume1 from '../../dileepspatel-070723.pdf'
 import AOS from 'aos';
 import "aos/dist/aos.css";
 
-// const resume_PDF = 'http://localhost:3000/dileepspatel-070723.pdf'
 
 
 const Header = () => {
-
-// const downloadFileAtUrl = (url) => {
-//   const fileName = url.split('/').pop()
-//   const aTag = document.createElement('a')
-//   aTag.href = url
-//   aTag.setAttribute('download',fileName)
-//   document.appendChild(aTag)
-//   aTag.click()
-//   aTag.remove()
-
-// } =
 
 
   useEffect(() => {
