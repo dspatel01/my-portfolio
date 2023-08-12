1b536c76fcac639a9e1f05fa98f42b746bbdc8c5
dileep <pateldileep290492@gmail.com>
2023-08-12 12:39:45 +0530

    homepage modification

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import ScrollToTop from './components/ScrollToTop';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
   <ScrollToTop />
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 
